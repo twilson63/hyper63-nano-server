c08ecddba61b7c7ffb9c692d1dc75b84810f4d33
Tom Wilson <twilson63@gmail.com>
2021-02-24 15:47:26 -0500

    removed minisearch and fs

--- hyper63.config.js
+++ hyper63.config.js
@@ -1,17 +1,13 @@
 const express = require('@hyper63/app-express')
 const pouchdb = require('@hyper63/adapter-pouchdb')
 const memory = require('@hyper63/adapter-memory')
-const fs = require('@hyper63/adapter-fs')
-const minisearch = require('@hyper63/adapter-minisearch')
 const jwt = require('./plugins/jwt')
 
 module.exports = {
   app: express,
   adapters: [
     { port: 'cache', plugins: [memory()]},
-    { port: 'data', plugins: [pouchdb({dir: process.env.DATA })] },
-    { port: 'storage', plugins: [fs({dir: process.env.DATA })]},
-    { port: 'search', plugins: [minisearch()]},
+    { port: 'data', plugins: [pouchdb({dir: process.env.DATA })] }
   ],
   middleware: [jwt]
 
