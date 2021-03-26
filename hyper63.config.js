6514f3797abf076bfac1e9fb3256fd59fc4dd3be
Tom Wilson <twilson63@gmail.com>
2021-03-26 14:42:17 -0400

    added queue service to nano

--- hyper63.config.js
+++ hyper63.config.js
@@ -1,13 +1,15 @@
 const express = require('@hyper63/app-express')
 const memory = require('@hyper63/adapter-memory')
 const pouchdb = require('@hyper63/adapter-pouchdb')
+const zmq = require('@hyper63/adapter-zmq')
 const jwt = require('./plugins/jwt')
 
 module.exports = {
   app: express,
   adapters: [
     { port: 'cache', plugins: [memory()]},
-    { port: 'data', plugins: [pouchdb({dir: process.env.DATA})]}
+    { port: 'data', plugins: [pouchdb({dir: process.env.DATA})]},
+    { port: 'queue', plugins: [zmq('7373')]
   ],
   middleware: [jwt]
 
